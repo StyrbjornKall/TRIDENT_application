27ca3e6c1c2e710ea25502ac42982454aafb5e50
StyrbjornKall <skall@chalmers.se>
2022-11-22 17:04:04 +0100

    Added some text

--- documentation_page.py
+++ documentation_page.py
@@ -1,12 +1,6 @@
 import streamlit as st
 
 def print_doc_page():
-    st.markdown('''
-# fishbAIT
-A deep learning software for chemical ecotoxicity predictions in fish'''
-)
+    
+    st.title('Documentation')
 
-if __name__ == '__main__':
-    # test1.py executed as script
-    # do something
-    print_doc_page()
